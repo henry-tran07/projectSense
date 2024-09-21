49f6bed9f1fe9548cac12c9c1eb24e1bf208aff4
Henry Tran <henryctran2007@gmail.com>
2024-09-20 20:57:28 -0500

    home v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { FcGoogle } from "react-icons/fc";
 import { auth, db } from "@/firebase/config";
 import TextField from "@mui/material/TextField";
-
+import ParticlesBackground from "./components/ParticleBackground";
 import {
   GoogleAuthProvider,
   signInWithEmailAndPassword,
@@ -69,99 +69,64 @@ const Home = () => {
   };
 
   return (
-    // <main className="flex-col w-screen h-screen flex items-center justify-center bg-orange-300">
-    //   <div className="absolute w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] md:w-[18rem] md:h-[18rem] lg:w-[20rem] lg:h-[20rem] animate-fadeIn">
-    //     <Image
-    //       src="/projectSenseLogo.png"
-    //       alt="Project Sense Logo"
-    //       layout="responsive"
-    //       width={512}
-    //       height={512}
-    //       priority={true}
-    //     />
-    //   </div>
-    //   {visible ? (
-    //     <>
-    //       <h1 className="animate-slideUp absolute top-12 font-sans text-orange-300 p-4 rounded-xl bg-white font-bold text-4xl">
-    //         Project Sense
-    //       </h1>
-    //       <div className="shadow-2xl bg-white w-72 h-[21.7rem] font-sans rounded-2xl text-center animate-slideUp">
-    //         <div className="text-center w-full font-semibold pt-8 pb-4 text-2xl">
-    //           Login
-    //         </div>
-    //         <form onSubmit={onSubmit}>
-    //           <input
-    //             type="text"
-    //             onChange={(e) => setEmail(e.target.value)}
-    //             placeholder="Email"
-    //             className="shadow-lg rounded-lg focus:outline-none border-gray-300 p-3 border-b-2 mx-auto w-10/12 h-fit text-xl"
-    //           />
-    //           <input
-    //             type="password"
-    //             onChange={(e) => setPassword(e.target.value)}
-    //             placeholder="Password"
-    //             className="shadow-lg rounded-lg mt-2 focus:outline-none border-gray-300 p-3 border-b-2 w-10/12 h-fit text-xl"
-    //           />
-    //           <div className="text-sm italic mx-auto my-1 underline text-center w-10/12 ">
-    //             <a href="/register">Don&apos;t have an account?</a>
-    //           </div>
-    //           <div className="mt-1 hover:bg-slate-800 bg-black text-white w-fit mx-auto px-3 py-2 rounded-2xl">
-    //             <button type="submit">Login</button>
-    //           </div>
-    //         </form>
-    //         <hr className="my-2 mx-auto w-10/12" />
-    //         <button
-    //           onClick={handleGoogleSignIn}
-    //           className="hover:bg-gray-200 bg-white mt-4 items-center w-10/12 mx-auto text-xl flex py-2 rounded-2xl gap-x-2 justify-center font-serif border-[1px] border-black"
-    //         >
-    //           <FcGoogle className="" />
-    //           <span className="font-sans">Sign up with Google</span>
-    //         </button>
-    //       </div>
-    //     </>
-    //   ) : (
-    //     <></>
-    //   )}
-    // </main>
-    <main className="flex flex-row w-screen h-screen bg-orange-300">
-      <div className=" bg-white w-[70%]  overflow-y-clip">
-        <div className="font-sans text-5xl font-bold h-screen w-full flex flex-col items-center justify-center text-orange-300">
+    <main
+      style={{
+        position: "relative", // Ensure positioning for z-index to work
+        zIndex: 1,
+      }}
+      className="flex flex-row w-screen h-screen"
+    >
+      <div className="bg-transparent xl:w-[80%] w-[70%] overflow-y-clip">
+        
+        <div className="font-sans text-6xl font-bold h-screen w-full flex flex-col items-center justify-center text-orange-400">
           <h1>Project Sense</h1>
           <p className="text-xl font-normal mt-2">
             Your go-to platform for mastering mental math and number sense
           </p>
         </div>
-        {/* <div className="h-screen flex flex-col items-center justify-center bg-orange-300 w-full">
-          <Image
-            src="/Project Sense Manual/ProjectSenseHomePage.png"
-            width={1400}
-            height={1000}
-            alt="Picture of the author"
-          />
-        </div> */}
       </div>
-      <div className="flex flex-col bg-orange-300 justify-center items-center w-[30%] h-screen">
+      <div className="flex flex-col bg-orange-300 justify-center items-center xl:w-[20%] w-[30%] h-screen">
         <div className="flex flex-col gap-y-6 items-center justify-center w-[90%] bg-white p-4 rounded-2xl">
-          <h1 className="text-4xl font-sans font-bold mt-16 ">Login</h1>
-          <TextField
-            id="standard-basic"
-            className="w-[80%] font-bold text-white"
-            label="Email"
-            variant="standard"
-          />
-          <TextField
-            id="standard-basic"
-            className="w-[80%]  text-white"
-            label="Password"
-            variant="standard"
-          />
-          <a
-            href="/register"
-            className="underline hover:decoration-2 w-[80%] text-md text-left "
+          <h1 className="text-4xl font-sans font-bold mt-16">Login</h1>
+          <form onSubmit={onSubmit} className="gap-y-6 w-full flex flex-col items-center">
+            <TextField
+              id="email"
+              className="w-[80%] font-bold text-white"
+              label="Email"
+              variant="standard"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} // Store email in state
+            />
+            <TextField
+              id="password"
+              className="w-[80%] text-white"
+              label="Password"
+              variant="standard"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} // Store password in state
+            />
+            <a
+              href="/register"
+              className="underline hover:decoration-2 w-[80%] text-md text-left"
+            >
+              Don&apos;t have an account?
+            </a>
+            <button
+              type="submit"
+              className="btn bg-orange-300 w-[80%] hover:bg-orange-400 text-white font-sans"
+            >
+              Login
+            </button>
+          </form>
+          <button
+            onClick={handleGoogleSignIn}
+            className="mb-16 hover:scale-105 ease-in-out duration-200 hover:bg-gray-200 bg-white items-center w-[80%] mx-auto text-xl flex py-2 rounded-2xl gap-x-2 justify-center font-serif border-[1px] border-black"
           >
-            Don&apos;t have an account?
-          </a>
-          <button className="btn btn-warning">Warning</button >       </div>
+            <FcGoogle className="" />
+            <span className="font-sans">Sign up with Google</span>
+          </button>
+        </div>
       </div>
     </main>
   );
